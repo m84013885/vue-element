1f4af9737254010ea4b60b72440ec567afcc74eb
黄启明 <huangqiming@qiushibaike.com>
2018-09-27 17:01:12 +0800

    fix : 删掉多余的内容

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,21 @@
 <template>
-  <el-container direction="vertical">
+  <div id="app">
 
-    <header_/>  
-    <el-container>
-      <aside_/>
-      <el-container direction="vertical">
-        <router-view></router-view>
-        <footer_/>
+    <el-container direction="vertical">
+      <header_/>  
+      <el-container>
+        <aside_/>
+        <el-container direction="vertical">
+          <router-view></router-view>
+          <footer_/>
+        </el-container>
       </el-container>
     </el-container>
 
-  </el-container>
-
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 import header_ from './components/common/header.vue'
 import footer_ from './components/common/footer.vue'
 import aside_ from './components/common/aside.vue'
@@ -23,7 +23,6 @@ import aside_ from './components/common/aside.vue'
 export default {
   name: 'app',
   components: {
-    HelloWorld,
     header_,
     footer_,
     aside_
